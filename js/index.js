acc26b720b700e4c4ee26ec0ef62732587801d70
devonk15 <dazzlingdumpsbusiness@gmail.com>
2024-07-27 22:08:29 -0400

    done and dusted

--- js/index.js
+++ js/index.js
@@ -5,14 +5,5 @@ function forward() {
     history.forward();
 }
 
-function alert1() {
-    var txt;
-    if (confirm("Press a button to continue!")) {
-        txt = "OK";
-    } else {
-        txt = "CANCELLED"
-    }
-    document.getElementById("result").innerHTML = txt;
-}
 
 

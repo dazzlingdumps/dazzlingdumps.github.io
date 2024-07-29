39436e4f437deb3fc68c4c25f28c3c317f48398f
devonk15 <dazzlingdumpsbusiness@gmail.com>
2024-07-28 21:00:13 -0400

    done and dusted

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 // This is for scrolling up faster
-
 window.onload = function scrollUp() {
     var scroll = document.getElementById("up")
     scroll.addEventListener("click", e => {
+        location.href=".";
+
         console.log("scrolled");
-        window.scrollTo(0, 0);
     });
 }
+
+// window.scroll(0, 0);

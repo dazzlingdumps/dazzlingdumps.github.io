18912f90d6f584e4b67c9db47910e0d0d774fde4
devonk15 <dazzlingdumpsbusiness@gmail.com>
2024-08-03 23:50:42 -0400

    update & delete candles.js

--- js/admin.js
+++ js/admin.js
@@ -1,20 +1,14 @@
 // This is for scrolling up faster
-/*window.onload = function scrollUp() {
-    var scroll = document.getElementById("up")
-    scroll.addEventListener("click", scroll1);
-
-    if (scroll.click()) {
-        function scroll1() {
-            console.log("scrolled");
-            scroll.removeEventListener("click", scroll1());
-        }
-    }
-}*/
+function scrollUp() {
+    document.body.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
 
 function goToWebsite() {
-    var btn = document.getElementById("btn");
     console.log("went");
-    btn.onclick = window.open(
+    window.open(
         "https://facebook.com",
         '_blank'
     );
--- js/candles.js
+++
@@ -1,8 +0,0 @@
-// This is for scrolling up faster
-/*window.onload = function scrollUp() {
-    var scroll = document.getElementById("up");
-    scroll.addEventListener("click", e => {
-        location.href = "./candles";
-        console.log("scrolled");
-    });
-}*/
